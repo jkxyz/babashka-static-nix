d6f7d771078c9fa76a2055f78c4d6d389abe8006
Josh Kingsley <josh@joshkingsley.me>
2022-02-09 00:20:58 +0200

    Add hashes

--- babashka-static.nix
+++ babashka-static.nix
@@ -13,13 +13,13 @@ let
     aarch64-linux = fetchurl {
       url =
         "https://github.com/babashka/babashka/releases/download/v${version}/babashka-${version}-linux-aarch64-static.tar.gz";
-      sha256 = "";
+      sha256 = "sha256-092w5av31gvwrgiiy40gjbm6kjpmipmya8i63dmqlbnhnwpsixvr";
     };
 
     x86_64-darwin = fetchurl {
       url =
         "https://github.com/babashka/babashka/releases/download/v${version}/babashka-${version}-macos-amd64.tar.gz";
-      sha256 = "";
+      sha256 = "sha256-02jpg91bk946895yy2gjkw2xivggz8rvc0937wkmsxwzbm5qn68q";
     };
 
     aarch64-darwin = x86_64-darwin;
@@ -35,5 +35,11 @@ in stdenv.mkDerivation {
   installPhase = ''
     mkdir -p $out/bin
     cp bb $out/bin
+  '';
+
+  doInstallCheck = true;
+
+  installCheckPhase = ''
+    $out/bin/bb --version | grep '${version}'
   '';
 }
